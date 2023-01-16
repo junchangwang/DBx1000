5a358f3384794573b945453a97599e8619cf5410
Shaozhe Guo <542409056@qq.com>
2023-01-16 11:49:11 +0800

    Update gnuplot

--- gnuplot-scripts/DBx_core.gnu
+++ gnuplot-scripts/DBx_core.gnu
@@ -18,3 +18,65 @@ plot "../dat_DBx/core.dat" every 4::0::24 title "Scan" lc rgb "purple" ps 1 pt 9
 set terminal eps font ',15'
 set output "../graphs_DBx/core/core.eps"
 replot
+
+###################################### core_f
+reset
+set size 0.99,1.0
+#set term pdf font ",10" size 5.2, 2.4
+set ylabel "Latency (ms)" offset 1.5,0,0
+set xlabel "Number of cores"
+set xtics ("1" 1, "2" 2 1, "4" 4, "8" 8, "16" 16, "24" 24, "32" 32)
+#set yrange [0:40]
+set xrange [0:35]
+set terminal jpeg font ',15'
+set output "../graphs_DBx/core/core_f.jpeg"
+set key top left
+
+plot "../dat_DBx/core_f.dat" every 4::0::24 title "Scan" lc rgb "purple" ps 1 pt 9 with linespoints,\
+      "../dat_DBx/core_f.dat" every 4::1::25 title "Hash" lc rgb "blue" ps 1 pt 8 with linespoints,\
+      "../dat_DBx/core_f.dat" every 4::2::26 title "BTree" lc rgb "green" ps 1 pt 7 with linespoints,\
+      "../dat_DBx/core_f.dat" every 4::3::27 title "CUBIT" lc rgb "red" ps 1 pt 6 with linespoints
+
+set terminal eps font ',15'
+set output "../graphs_DBx/core/core_f.eps"
+replot
+
+############################## histograms###############################
+reset
+set key invert top left
+
+set terminal jpeg font ',15'
+set output "../graphs_DBx/core/histograms.jpeg"
+
+set origin 0,0.05
+set size 1,0.95
+
+#set yrange [0:3e3]
+set autoscale y
+
+#set title "Breakdown of Updates"
+set style data histograms
+set style histogram rowstacked
+set boxwidth 0.75 relative
+set style fill solid 1.0 border -1
+set xtics nomirror rotate by -45 scale 0
+
+set ylabel "Latency (s)" offset 1,0,0
+
+#unset xtics
+set label "1" font ",13" at screen 0.25, screen 0.04
+set label "2" font ",13" at screen 0.35, screen 0.04
+set label "4" font ",13" at screen 0.45, screen 0.04
+set label "8" font ",13" at screen 0.55, screen 0.04
+set label "16" font ",13" at screen 0.65, screen 0.04
+set label "24" font ",13" at screen 0.75, screen 0.04
+set label "32" font ",13" at screen 0.85, screen 0.04
+
+set xtics font ",8"
+
+plot "../dat_DBx/index_time.dat" using ($3):xtic(4) title "Row" with histograms fill pattern 4 border rgb "black" lc rgb "black", \
+     "../dat_DBx/index_time.dat" using ($2):xtic(4) title "Index" with histograms fs solid border rgb "black" lc rgb "black"
+
+set terminal eps font ',15'
+set output "../graphs_DBx/core/histograms.eps"
+replot
--- run_sz_DBx.py
+++ run_sz_DBx.py
@@ -40,8 +40,6 @@ def latency_f_analysis(filename):
         a = line.split()
         if len(a) != 5:
             continue
-        # elif not str(a[-2]).isdigit():
-        #     continue
         elif line.startswith('Hash '):
             Hvec.append(float(a[-2]))
         elif line.startswith('BTree '):
@@ -100,8 +98,103 @@ def memory_analysis(filename):
     ret.append(bitmap_memory)
     return ret
 
+def index_time_analysis(filename):
+    f = open(filename)     
+    Hvec = [] # hash
+    Bvec = [] # btree
+    Cvec = [] # cubit
+
+    Hvec_f = [] # hash, index time
+    Bvec_f = [] # btree
+    Cvec_f = [] # cubit
+
+    ret = []
+
+    for line in f:
+        a = line.split()
+        if len(a) != 5:
+            continue
+        elif line.startswith('Hash '):
+            Hvec.append(float(a[-1]))
+            Hvec_f.append(float(a[-2]))
+        elif line.startswith('BTree '):
+            Bvec.append(float(a[-1]))
+            Bvec_f.append(float(a[-2]))
+        elif line.startswith('CUBIT '):
+            Cvec.append(float(a[-1]))
+            Cvec_f.append(float(a[-2]))
+        else:
+            continue
+
+    if len(Hvec_f) != 0:
+        ret.append(sum(Hvec_f) / len(Hvec_f)) 
+    else:
+        ret.append(0)
+    if len(Hvec) != 0:
+        ret.append(sum(Hvec) / len(Hvec)) 
+    else:
+        ret.append(0)
+
+    if len(Bvec_f) != 0:
+        ret.append(sum(Bvec_f) / len(Bvec_f)) 
+    else:
+        ret.append(0)
+    if len(Bvec) != 0:
+        ret.append(sum(Bvec) / len(Bvec)) 
+    else:
+        ret.append(0)
+
+    if len(Cvec_f) != 0:
+        ret.append(sum(Cvec_f) / len(Cvec_f)) 
+    else:
+        ret.append(0)
+    if len(Cvec) != 0:
+        ret.append(sum(Cvec) / len(Cvec)) 
+    else:
+        ret.append(0)
+    return ret
+
+
+def  run_new_figure():
+    f = open('dat_DBx/index_time.dat', 'w')
+    core_number = [1, 2, 4, 8, 16, 24, 32]
+    ret = [[]]
+    ret.clear()
+    for num in core_number:
+        ret.append(index_time_analysis('dat_tmp_DBx/core_{}.dat'.format(num)))
+
+    f.write('1  {0} {1} "Hash"\n'.format(ret[0][0], ret[0][1]))
+    f.write('2  {0} {1} "BTree"\n'.format(ret[0][2], ret[0][3]))
+    f.write('3  {0} {1} "CUBIT"\n'.format(ret[0][4], ret[0][5]))
+    f.write('4  0         0 ""\n')
+    f.write('5  {0} {1} "Hash"\n'.format(ret[1][0], ret[1][1]))
+    f.write('6  {0} {1} "BTree"\n'.format(ret[1][2], ret[1][3]))
+    f.write('7  {0} {1} "CUBIT"\n'.format(ret[1][4], ret[1][5]))
+    f.write('8  0         0 ""\n')
+    f.write('9  {0} {1} "Hash"\n'.format(ret[2][0], ret[2][1]))
+    f.write('10  {0} {1} "BTree"\n'.format(ret[2][2], ret[2][3]))
+    f.write('11  {0} {1} "CUBIT"\n'.format(ret[2][4], ret[2][5]))
+    f.write('12  0         0 ""\n')
+    f.write('13  {0} {1} "Hash"\n'.format(ret[3][0], ret[3][1]))
+    f.write('14  {0} {1} "BTree"\n'.format(ret[3][2], ret[3][3]))
+    f.write('15  {0} {1} "CUBIT"\n'.format(ret[3][4], ret[3][5]))
+    f.write('16  0         0 ""\n')
+    f.write('17  {0} {1} "Hash"\n'.format(ret[4][0], ret[4][1]))
+    f.write('18  {0} {1} "BTree"\n'.format(ret[4][2], ret[4][3]))
+    f.write('19  {0} {1} "CUBIT"\n'.format(ret[4][4], ret[4][5]))
+    f.write('20  0         0 ""\n')
+    f.write('21  {0} {1} "Hash"\n'.format(ret[5][0], ret[5][1]))
+    f.write('22  {0} {1} "BTree"\n'.format(ret[5][2], ret[5][3]))
+    f.write('23  {0} {1} "CUBIT"\n'.format(ret[5][4], ret[5][5]))
+    f.write('24  0         0 ""\n')
+    f.write('25  {0} {1} "Hash"\n'.format(ret[6][0], ret[6][1]))
+    f.write('26  {0} {1} "BTree"\n'.format(ret[6][2], ret[6][3]))
+    f.write('27  {0} {1} "CUBIT"\n'.format(ret[6][4], ret[6][5]))
+    f.close()    
+
 def run():
     gen_raw_data()
+
     print ('DBx1000 core')
     print ('-' * 10)
     core_number = [1, 2, 4, 8, 16, 24, 32]
@@ -135,6 +228,9 @@ def run():
             f.write('{} {} \n'.format(num, tp))
     f.close()  
 
+    print ('-' * 10)
+    # new_figure
+    run_new_figure()
 
 def gen_graph():
     os.chdir("gnuplot-scripts")
@@ -143,7 +239,6 @@ def gen_graph():
     # os.system("rm -r ../graphs")
     os.system("make make_dir_DBx")
     os.system("make figure_DBx_core")
-    os.system("make figure_DBx_core_f")
     os.chdir("../graphs_DBx")
     os.system('echo "Figures generated in \"`pwd`\""')
     #os.system('ls -l')
