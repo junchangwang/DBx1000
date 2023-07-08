a2bb2b70368f6124ae0ef1995c5b57d59321c050
chienguo <2778180460@qq.com>
2023-07-08 10:40:47 +0800

    fix index_size in index_hash

--- storage/index_hash.cpp
+++ storage/index_hash.cpp
@@ -29,8 +29,8 @@ int IndexHash::index_size()
 		for (int j = 0; j < bucket_cnt_per_part; j++) {
 				bucket = &_buckets[i][j];
 				cur_node = bucket->first_node;
-				size += sizeof(bucket);
-				size += sizeof(cur_node);
+				size += sizeof(*bucket);
+				size += sizeof(*cur_node);
 				while (cur_node != NULL) {
 					item = cur_node->items;
 					while (item != NULL) {
@@ -39,6 +39,7 @@ int IndexHash::index_size()
 						item = item->next;
 					}
 					cur_node = cur_node->next;
+					size += sizeof(*cur_node);
 				}
 		}
 	}
