29ddeedbdac147ec36143af47585a69fe7651b79
chienguo <2778180460@qq.com>
2023-07-08 11:43:53 +0800

    Define ORDERED_LEAF_LIST in config.h

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#define ORDERED_LEAF_LIST 1
+
 /***********************************************/
 // Simulation + Hardware
 /***********************************************/
