be7e8afe640198e3080da54ab973dc41f2978e11
Junchang Wang <junchang@T470s>
2023-07-09 09:23:35 +0800

    Update DBx_core.py

--- DBx_core.py
+++ DBx_core.py
@@ -71,7 +71,7 @@ co_map = {
     'BTree'  :   'brown',
     'CUBIT'  :   'blue',
     'B-Tree' :   'c',
-    'Trie'   :   'r'
+    'Trie'   :   'tan'
 }
 
 mark_map = {
@@ -118,42 +118,40 @@ def draw():
     gs = gridspec.GridSpec(2, 1, height_ratios = [1.8, 1], hspace = 0.3)
     ax1 = plt.subplot(gs[0, 0])
     ax2 = plt.subplot(gs[1, 0])
-    ax1.tick_params(labelsize = 18)
-    ax2.tick_params(labelsize = 18)
+    ax1.tick_params(labelsize = 22)
+    ax2.tick_params(labelsize = 22)
     
     #### core plot
     for exp in algorithm_lookup_1:
         ax1.plot(x_val, list_map[exp], marker = mark_map[exp], ms = 11, mfc = 'w', c = co_map[exp], ls = ls_map[exp], lw = 2, label = label_nm_map[exp])
         
     
-    ax1.set_ylabel("Throughput (op/s)", fontsize= 20)
-    ax1.set_xlabel("(a) Number of cores", fontsize= 20)
+    ax1.set_ylabel("Throughput (op/s)", fontsize= 22)
+    ax1.set_xlabel("Number of cores", fontsize= 22)
     ax1.set_xticks(x_val)
-    ax1.legend(loc = 'upper left', fontsize = 16, frameon = False)
+    ax1.legend(loc = 'upper left', fontsize = 18, frameon = False)
     
     #### memory bar
-    ax2.set_ylim(0, 2.2)
+    ax2.set_ylim(0, 2.4)
     ax2.set_yticks([0, 0.5, 1, 1.5, 2])
     
-    ax2.bar('Hash', Bar_val[0], lw = 0.8, fc = 'g', width = 0.3) 
-    ax2.bar('$\mathregular{B^+}$-Tree ', Bar_val[1], lw = 0.8, fc = 'brown', width = 0.3)
-    ax2.bar('Trie', Bar_val[3], lw = 0.8, fc = 'r', width = 0.3)
-    ax2.bar('CUBIT', Bar_val[4], lw = 0.8, fc = 'blue', width = 0.3)
+    ax2.bar('Hash', Bar_val[0], lw = 0.8, fc = 'g', width = 0.5) 
+    ax2.bar('$\mathregular{B^+}$-Tree ', Bar_val[1], lw = 0.8, fc = 'brown', width = 0.5)
+    ax2.bar('Trie', Bar_val[3], lw = 0.8, fc = 'tan', width = 0.5)
+    ax2.bar('Ours', Bar_val[4], lw = 0.8, fc = 'blue', width = 0.5)
     
-    ax2.text(0, Bar_val[0] + 0.05, round(Bar_val[0], 2), ha = 'center', va = "bottom", fontsize = 15)
-    ax2.text(1, Bar_val[1] + 0.05, round(Bar_val[1], 2), ha = 'center', va = "bottom", fontsize = 15)
-    ax2.text(3, Bar_val[4] + 0.05, round(Bar_val[4], 2), ha = 'center', va = "bottom", fontsize = 15)
+    ax2.text(0, Bar_val[0] + 0.05, round(Bar_val[0], 2), ha = 'center', va = "bottom", fontsize = 20)
+    ax2.text(1, Bar_val[1] + 0.05, round(Bar_val[1], 2), ha = 'center', va = "bottom", fontsize = 20)
+    ax2.text(2, Bar_val[3] + 0.05, round(Bar_val[3], 2), ha = 'center', va = "bottom", fontsize = 20)
+    ax2.text(3, Bar_val[4] + 0.05, round(Bar_val[4], 2), ha = 'center', va = "bottom", fontsize = 20)
     
-    ax2.set_ylabel("Memory (GB)", fontsize = 20)
-    ax2.set_xlabel("(b) algorithm type", fontsize = 20)
+    ax2.set_ylabel("Memory Size (GB)", fontsize = 22)
+    # ax2.set_xlabel("(b) Different Indexes", fontsize = 20)
               
               
     #plt.show()
     
     plt.savefig(dir + '/graphs_DBx_1/graphs_DBx/mix.eps', format='eps', dpi= 1200)
-    
-    
-    
     
 get_data()  
 draw()
